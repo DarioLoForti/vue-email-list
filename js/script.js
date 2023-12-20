ae385375ae216570c81630dcf6fd351cacb2ba30
DarioLoForti <lofortidario@gmail.com>
2023-12-20 15:53:44 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -3,10 +3,21 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-
+            emails: [] ,
         }
     },
     methods: {
+        generateArray() {
+            for(let i=0; i<10; i++){
+
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+
+                    this.emails.push(response.data.response) ;
+
+                });
+            }
+        },
         
+       
     },
-}).mount("#app")
+}).mount("#app");
